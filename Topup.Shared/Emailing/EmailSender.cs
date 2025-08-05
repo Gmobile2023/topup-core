49e027b5152029c5d17424ad9e40715f2d87ed81
gunner <letienhoang@hotmail.com>
2025-08-05 15:24:37 +0700

    test email

--- Topup.Shared/Emailing/EmailSender.cs
+++ Topup.Shared/Emailing/EmailSender.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Mail;
 using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using MimeKit;
 
 namespace Topup.Shared.Emailing;
 
@@ -40,7 +42,7 @@ public class EmailSender : IEmailSender
                 $"Kho thẻ {stockCode} nhà mạng {stockType} sản phẩm {productCode} sắp hết. Tồn kho hiện tại còn: {inventory}. Vui lòng bổ sung thêm thẻ vào kho";
             mailMessage.AppendLine(msgBody);
             foreach (var item in emails)
-                ReplaceBodyAttachmentsAndSend(item,
+                _ = ReplaceBodyAttachmentsAndSend(item,
                     $"Email cảnh báo kho thẻ {stockCode}-{stockType}-{productCode} sắp hết", emailTemplate,
                     mailMessage);
 
@@ -85,7 +87,8 @@ public class EmailSender : IEmailSender
             var mailMessage = new StringBuilder();
             mailMessage.AppendLine(msgBody);
             foreach (var item in emails)
-                ReplaceBodyAttachmentsAndSend(item, title, emailTemplate, mailMessage, !string.IsNullOrEmpty(linkAddtach) ? new[] { linkAddtach } : null);
+                _ = ReplaceBodyAttachmentsAndSend(item, title, emailTemplate, mailMessage,
+                    !string.IsNullOrEmpty(linkAddtach) ? new[] { linkAddtach } : null);
 
             return true;
         }
@@ -104,51 +107,114 @@ public class EmailSender : IEmailSender
         return emailTemplate;
     }
 
-    private void ReplaceBodyAttachmentsAndSend(string emailAddress, string subject,
+    // private void ReplaceBodyAttachmentsAndSend(string emailAddress, string subject,
+    //     StringBuilder emailTemplate,
+    //     StringBuilder mailMessage, IReadOnlyCollection<string> pathAttachments = null)
+    // {
+    //     try
+    //     {
+    //         var client = new SmtpClient(_configuration["EmailConfig:SmtpServer"])
+    //         {
+    //             UseDefaultCredentials = false,
+    //             Credentials = new NetworkCredential(_configuration["EmailConfig:EmailAddress"],
+    //                 _configuration["EmailConfig:EmailPassword"]),
+    //             Port = int.Parse(_configuration["EmailConfig:Port"]),
+    //             EnableSsl = bool.Parse(_configuration["EmailConfig:EnableSsl"])
+    //         };
+    //         emailTemplate.Replace("{EMAIL_BODY}", mailMessage.ToString());
+    //         var mess = new MailMessage
+    //         {
+    //             To = { emailAddress },
+    //             Subject = subject,
+    //             Body = emailTemplate.ToString(),
+    //             IsBodyHtml = true,
+    //             From = new MailAddress(_configuration["EmailConfig:EmailAddress"],
+    //                 _configuration["EmailConfig:EmailDisplay"])
+    //         };
+    //         if (pathAttachments != null && pathAttachments.Any())
+    //             foreach (var item in pathAttachments)
+    //                 mess.Attachments.Add(new Attachment(item));
+    //
+    //         try
+    //         {
+    //             client.Send(mess);
+    //         }
+    //         catch (Exception e)
+    //         {
+    //             throw;
+    //         }
+    //         finally
+    //         {
+    //             mess.Dispose();
+    //             client.Dispose();
+    //         }
+    //     }
+    //     catch (Exception ex)
+    //     {
+    //         _logger.LogError($"{emailAddress} ReplaceBodyAttachmentsAndSend error: {ex}");
+    //     }
+    // }
+    private async Task ReplaceBodyAttachmentsAndSend(
+        string emailAddress,
+        string subject,
         StringBuilder emailTemplate,
-        StringBuilder mailMessage, IReadOnlyCollection<string> pathAttachments = null)
+        StringBuilder mailMessage,
+        IReadOnlyCollection<string> pathAttachments = null)
     {
         try
         {
-            var client = new SmtpClient(_configuration["EmailConfig:SmtpServer"])
-            {
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(_configuration["EmailConfig:EmailAddress"],
-                    _configuration["EmailConfig:EmailPassword"]),
-                Port = int.Parse(_configuration["EmailConfig:Port"]),
-                EnableSsl = bool.Parse(_configuration["EmailConfig:EnableSsl"])
-            };
-            emailTemplate.Replace("{EMAIL_BODY}", mailMessage.ToString());
-            var mess = new MailMessage
-            {
-                To = { emailAddress },
-                Subject = subject,
-                Body = emailTemplate.ToString(),
-                IsBodyHtml = true,
-                From = new MailAddress(_configuration["EmailConfig:EmailAddress"],
-                    _configuration["EmailConfig:EmailDisplay"])
-            };
-            if (pathAttachments != null && pathAttachments.Any())
-                foreach (var item in pathAttachments)
-                    mess.Attachments.Add(new Attachment(item));
+            var message = new MimeMessage();
 
-            try
+            // From
+            message.From.Add(new MailboxAddress(
+                _configuration["EmailConfig:EmailDisplay"],
+                _configuration["EmailConfig:EmailAddress"]
+            ));
+
+            // To
+            message.To.Add(MailboxAddress.Parse(emailAddress));
+            message.Subject = subject;
+
+            // Body
+            emailTemplate.Replace("{EMAIL_BODY}", mailMessage.ToString());
+
+            var builder = new BodyBuilder
             {
-                client.Send(mess);
-            }
-            catch (Exception e)
+                HtmlBody = emailTemplate.ToString()
+            };
+
+            // Attachments
+            if (pathAttachments != null && pathAttachments.Any())
             {
-                throw;
+                foreach (var filePath in pathAttachments)
+                {
+                    builder.Attachments.Add(filePath);
+                }
             }
-            finally
-            {
-                mess.Dispose();
-                client.Dispose();
-            }
+
+            message.Body = builder.ToMessageBody();
+
+            // SMTP client
+            using var client = new SmtpClient();
+
+            // Optional: override HELO name (fix "Invalid HELO name" error)
+            client.LocalDomain = _configuration["EmailConfig:LocalDomain"]; // bạn có thể sửa theo nhu cầu
+
+            var smtpServer = _configuration["EmailConfig:SmtpServer"];
+            var port = int.Parse(_configuration["EmailConfig:Port"]);
+            var email = _configuration["EmailConfig:EmailAddress"];
+            var password = _configuration["EmailConfig:EmailPassword"];
+            var enableSsl = bool.Parse(_configuration["EmailConfig:EnableSsl"]);
+
+            await client.ConnectAsync(smtpServer, port,
+                enableSsl ? SecureSocketOptions.StartTls : SecureSocketOptions.None);
+            await client.AuthenticateAsync(email, password);
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
         }
         catch (Exception ex)
         {
-            _logger.LogError($"{emailAddress} ReplaceBodyAttachmentsAndSend error: {ex}");
+            _logger.LogError($"{emailAddress} ReplaceBodyAttachmentsAndSendAsync error: {ex}");
         }
     }
 }
